e8f9899751508bccedc58266e8979663ed827f9e
lucasguimaraes2005 <lucaagui123@gmail.com>
2024-04-19 11:38:20 -0300

    feat: fixed img

---
+++ script-crud.js
@@ -0,0 +1,152 @@
+// DECLARAÇÕES
+const btnAdicionarTarefa = document.querySelector('.app__button--add-task');
+const formAdicionarTarefa = document.querySelector('.app__form-add-task');
+const textArea = document.querySelector('.app__form-textarea');
+const ulTarefas = document.querySelector('.app__section-task-list');
+const btnDeletar = document.querySelector('.app__form-footer__button--delete')
+const btnCancelar = document.querySelector('.app__form-footer__button--cancel');
+const paragrafoDescricaoTarefa = document.querySelector('.app__section-active-task-description');
+const btnRemoverConcluidas = document.querySelector('#btn-remover-concluidas');
+const btnRemoverTodas = document.querySelector('#btn-remover-todas');
+
+let tarefas = JSON.parse(localStorage.getItem('tarefas')) || [];
+let tarefaSelecionada = null
+let liTarefaSelecionada = null
+
+// FUNÇÕES
+function atualizarTarefas () {
+    localStorage.setItem('tarefas', JSON.stringify(tarefas));
+};
+
+function criarElementoTarefa(tarefa) {
+    const li = document.createElement('li');
+    li.classList.add('app__section-task-list-item');
+
+    const svg = document.createElement('svg');
+    svg.innerHTML = `
+        <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none"
+            xmlns="http://www.w3.org/2000/svg">
+            <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
+            <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z"
+                fill="#01080E"></path>
+        </svg>
+    `;
+
+    const paragrafo = document.createElement('p');
+    paragrafo.textContent = tarefa.descricao;
+    paragrafo.classList.add('app__section-task-list-item-description');
+
+    const botao = document.createElement('button');
+    botao.classList.add('app_button-edit');
+    botao.onclick = () => {
+        const novaDescricao = prompt("Qual é o nono nome da tarefa?");
+        if (novaDescricao) {
+            paragrafo.textContent = novaDescricao;
+            tarefa.descricao = novaDescricao
+            atualizarTarefas()
+        }
+        
+    };
+
+    const imagemBotao = document.createElement('img');
+    imagemBotao.setAttribute('src', './imagens/edit.png');
+    botao.append(imagemBotao);
+
+    li.append(svg);
+    li.append(paragrafo);
+    li.append(botao);
+    
+    if (tarefa.completa) {
+        li.classList.add('app__section-task-list-item-complete');
+        botao.setAttribute('disabled', 'disabled');
+    } else {
+        li.onclick = () => {
+            document.querySelectorAll('.app__section-task-list-item-active').forEach(elemento => {elemento.classList.remove('app__section-task-list-item-active')});
+            
+            if (tarefaSelecionada == tarefa) {
+                paragrafoDescricaoTarefa.textContent = '';
+                tarefaSelecionada = null
+                liTarefaSelecionada = null
+                return
+            };
+    
+            tarefaSelecionada = tarefa;
+            liTarefaSelecionada = li
+            paragrafoDescricaoTarefa.textContent = tarefa.descricao;
+            li.classList.add('app__section-task-list-item-active');
+        };    
+    }
+    
+    return li;
+};
+
+function deletarTarefa(cancelar) {
+    textArea.value = '';
+    if (cancelar) {
+        formAdicionarTarefa.classList.add('hidden');
+    }
+}
+
+function removerTarefas(somenteCompletas) {
+    const seletor = somenteCompletas ? ".app__section-task-list-item-complete" : ".app__section-task-list-item";
+    
+    document.querySelectorAll(seletor).forEach(elemento => {
+        elemento.remove();
+    });
+    tarefas = somenteCompletas ? tarefas.filter(tarefa => !tarefa.completa) : [];
+    atualizarTarefas();
+};
+
+
+// EVENTOS
+btnRemoverConcluidas.onclick = () => removerTarefas(true);
+btnRemoverTodas.onclick = () => removerTarefas(false);
+
+btnAdicionarTarefa.addEventListener("click", () => {
+    formAdicionarTarefa.classList.remove('hidden');
+});
+
+document.onkeydown = (e) => {
+    if (e.code === "Escape" && !formAdicionarTarefa.classList.contains('hidden')) {
+        deletarTarefa(true)
+    }
+}
+
+formAdicionarTarefa.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const tarefa = {
+        descricao: textArea.value,
+    }
+    tarefas.push(tarefa);
+    const elementoTarefa = criarElementoTarefa(tarefa);
+    ulTarefas.append(elementoTarefa);
+    atualizarTarefas();
+    deletarTarefa(true)
+});
+
+
+
+tarefas.forEach(tarefa => {
+    const elementoTarefa = criarElementoTarefa(tarefa);
+    ulTarefas.append(elementoTarefa);
+});
+
+btnCancelar.addEventListener("click", () => {
+    deletarTarefa(true)
+});
+
+btnDeletar.addEventListener("click", () => {
+    deletarTarefa(false)
+});
+
+document.addEventListener('FocoFinalizado', () => {
+    if (tarefaSelecionada && liTarefaSelecionada) {
+        liTarefaSelecionada.classList.remove('app__section-task-list-item-active');
+        liTarefaSelecionada.classList.add('app__section-task-list-item-complete');
+        liTarefaSelecionada.querySelector('button').setAttribute('disabled', 'disabled');
+        tarefaSelecionada.completa = true;
+        atualizarTarefas();
+    };
+});
+
+
---
+++ script.js
@@ -0,0 +1,134 @@
+// DECLARAÇÕES
+const html = document.querySelector('html');
+const focoBt = document.querySelector('.app__card-button--foco');
+const curtoBt = document.querySelector('.app__card-button--curto');
+const longoBt = document.querySelector('.app__card-button--longo');
+const banner = document.querySelector('.app__image');
+const titulo = document.querySelector('.app__title');
+const botoes = document.querySelectorAll('.app__card-button');
+const startPauseBt = document.querySelector('#start-pause');
+const iniciarOuPausarBt = document.querySelector('#start-pause span')
+const musicaFocoInput = document.querySelector('#alternar-musica');
+const iconBt = document.querySelector('.app__card-primary-butto-icon');
+const tempoNaTela = document.querySelector('#timer');
+const musica = new Audio('./sons/luna-rise-part-one.mp3');
+const play = new Audio('./sons/play.wav');
+const pause = new Audio('./sons/pause.mp3');
+const beep = new Audio('./sons/beep.mp3');
+
+
+let tempoDecorridoEmSegundos = 1500;
+let intervaloId = null;
+
+musica.loop = true;
+
+//FUNÇÕES
+function alterarContexto(contexto) {
+    mostrarTempo();
+    botoes.forEach(function (contexto) {
+        contexto.classList.remove('active')
+    });
+    html.setAttribute('data-contexto', contexto);
+    banner.setAttribute('src', `./imagens/${contexto}.png`);
+    switch (contexto) {
+        case "foco":
+            titulo.innerHTML = `Otimize sua produtividade,<br>
+            <strong class="app__title-strong">mergulhe no que importa.</strong>`;
+
+            break;
+
+        case "descanso-curto":
+            titulo.innerHTML = `Que tal dar uma respirada?<br>
+            <strong class="app__title-strong">Faça uma pausa curta.</strong>`;
+
+            break;
+
+        case "descanso-longo":
+            titulo.innerHTML = `Hora de voltar à superfície<br>
+            <strong class="app__title-strong">Faça uma pausa longa.</strong>`;
+
+        default:
+            break;
+    }
+};
+
+const contagemRegressiva = () => {
+    if (tempoDecorridoEmSegundos <= 0) {
+        beep.play();
+        alert("Tempo finalizado!");
+        const focoAtivo = html.getAttribute('data-contexto') == 'foco';
+        if (focoAtivo) {
+            const evento = new CustomEvent('FocoFinalizado');
+            document.dispatchEvent(evento);
+        };
+        zerar();
+        return;
+    };
+    tempoDecorridoEmSegundos -= 1;
+    mostrarTempo();
+};
+
+function iniciarOuPausar() {
+    if(intervaloId) {
+        zerar();
+        pause.play();
+        return;
+    }
+    play.play();
+    iniciarOuPausarBt.textContent = 'Pausar';
+    iconBt.setAttribute('src', './imagens/pause.png')
+    intervaloId = setInterval(contagemRegressiva, 1000);
+};
+
+function zerar() {
+    clearInterval(intervaloId);
+    iniciarOuPausarBt.textContent = 'Começar';
+    iconBt.setAttribute('src', './imagens/play_arrow.png')
+    intervaloId = null;
+    if (focoBt.classList.contains('active') && tempoDecorridoEmSegundos === 0) {
+        tempoDecorridoEmSegundos = 1500;
+    } else if (curtoBt.classList.contains('active') && tempoDecorridoEmSegundos === 0) {
+        tempoDecorridoEmSegundos = 300;
+    } else if (longoBt.classList.contains('active') && tempoDecorridoEmSegundos === 0){
+        tempoDecorridoEmSegundos = 900;
+    }
+    
+    mostrarTempo();
+};
+
+function mostrarTempo() {
+    const tempo = new Date(tempoDecorridoEmSegundos * 1000);
+    const tempoFormatado = tempo.toLocaleTimeString('pt-Br', {minute: '2-digit', second: '2-digit'});
+    tempoNaTela.innerHTML = `${tempoFormatado}`;
+}
+
+mostrarTempo();
+
+//EVENTOS
+focoBt.addEventListener('click', () => {
+    tempoDecorridoEmSegundos = 1500;
+    alterarContexto('foco');
+    focoBt.classList.add('active');
+});
+
+curtoBt.addEventListener('click', () => {
+    tempoDecorridoEmSegundos = 300;
+    alterarContexto('descanso-curto');
+    curtoBt.classList.add('active');
+});
+
+longoBt.addEventListener('click', () => {
+    tempoDecorridoEmSegundos = 900;
+    alterarContexto('descanso-longo');
+    longoBt.classList.add('active');
+});
+
+musicaFocoInput.addEventListener('change', () => {
+    if(musica.paused) {
+        musica.play();
+    } else {
+        musica.pause();
+    }
+});
+
+startPauseBt.addEventListener('click', iniciarOuPausar);
